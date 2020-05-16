ee48f128ac1045594e4611b497eabde285e10b3e
Juan David Loaiza Botero <jloaiz16@eafit.edu.co>
2020-05-16 13:12:56 -0500

    Initial commit

---
+++ src/test/java/com/jdeveloper/client/management/backend/clientmanagementbackend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdeveloper.client.management.backend.clientmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
